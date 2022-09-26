113c6a07cce8da1943b42287ad6fb7d1fe08fb66
MohanaSupraja <102228446+MohanaSupraja@users.noreply.github.com>
2022-09-26 20:53:30 +0530

    Update diabeteswebapp.py

--- diabeteswebapp.py
+++ diabeteswebapp.py
@@ -8,7 +8,7 @@ Created on Sun Sep 25 18:41:52 2022
 import numpy as np
 import pickle
 import streamlit as st
-loaded_model=pickle.load(open('diabetes','rb'))
+loaded_model=pickle.load(open('diabetes.sav','rb'))
 #creating a function for prediction
 def diabetes_prediction(input_data):
     
